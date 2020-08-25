72b21d00d1de855ed8b8739c980c5887e6e5d76e
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-25 11:18:12 -0600

    Add project files.

---
+++ constants.vb
@@ -0,0 +1,26 @@
+﻿Option Strict Off
+Option Explicit On
+
+Module constants
+
+    Sub Main()
+        'like microsoft example
+        Const ANumber As Integer = 5I
+
+        'Robotics Style Convention 
+        ' Const ANOTHERNUMBER As Integer = 6I
+        'const ANOTHERNUMBER% = 6%
+        'Const ANOTHERNUMBER% = 6I
+        ' "AS"integer statement does thee same as I and %. Use AS for assignments for now.
+        'ALL CAPS IS A CLUE THAT ITS A CONSTANT AND NOT A VARIABLE  (in robotics convention)
+        'include type character 
+        'I indiscates constant
+
+        Console.WriteLine(ANOTHERNUMBER%)
+        '% indicates a constant as well
+
+
+
+    End Sub
+
+End Module
